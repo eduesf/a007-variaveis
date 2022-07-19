ce061f098e21b6b559842a618390cc307911efc0
Eduardo Fernandes <eduardo.eduesf@gmail.com>
2022-07-19 18:18:59 -0300

    Exercicios 1, 2 e 3 para a matéria de Variaveis e tipos primitivos

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const Perg1 = "Você gosta de comida Japonesa?"
+const Perg2 = "Você gosta de comida Mexicana?"
+const Perg3 = "Você gosta de Pizza?"
+
+const Resp1 = prompt (Perg1)
+const Resp2 = prompt (Perg2)
+const Resp3 = prompt (Perg3)
+
+console.log (Perg1, Resp1)
+console.log (Perg2, Resp2)
+console.log (Perg3, Resp3)
+
